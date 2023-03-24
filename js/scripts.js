f5451994f87689608a605bf84a3a47db5cc759c6
ChrisRDavila <crossdavila@gmail.com>
2023-03-24 11:07:48 -0700

    building business logic and branching results

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//UI Logic
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    hideResults();
+    result();
+    })
+  })    
+
+hideResults () {
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("swift").setAttribute("class", "hidden");
+  document.getElementById("rust").setAttribute("class", "hidden");
+  document.getElementById("go").setAttribute("class", "hidden");
+  document.getElementById("C##").setAttribute("class", "hidden");
+}
+ 
+//Business Logic
+
+function results(){
+  const qSnake = parseInt(document.querySelector("input[name=snake]:checked").value);
+  const  qGem= parseInt(document.querySelector("input[name=gem]:checked").value);
+  const qFast = parseInt(document.querySelector("input[name=fast]:checked").value);
+  const qOxy = parseInt(document.querySelector("input[name=oxy]:checked").value);
+  const qGreen = parseInt(document.querySelector("input[name=green]:checked").value);
+  documebt.querySelector("form").removeEventListener("submit", results);
+
+  if ()
+}
+
