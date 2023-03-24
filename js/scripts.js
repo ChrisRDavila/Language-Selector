9be29f17b17bca3c907d3f070b42012605d2bb45
ChrisRDavila <crossdavila@gmail.com>
2023-03-24 11:32:18 -0700

    debbuged error spelling in js and syntax issues, still lacks proper functionaliy but no syntax errors for now

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,29 @@ window.addEventListener("load", function() {
   document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
     hideResults();
-    result();
+    results();
     })
   })    
 
-hideResults () {
+function hideResults () {
   document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("ruby").setAttribute("class", "hidden");
   document.getElementById("swift").setAttribute("class", "hidden");
   document.getElementById("rust").setAttribute("class", "hidden");
   document.getElementById("go").setAttribute("class", "hidden");
-  document.getElementById("C##").setAttribute("class", "hidden");
+  document.getElementById("c##").setAttribute("class", "hidden");
 }
  
 //Business Logic
 
-function results(){
+function results() {
   const qSnake = parseInt(document.querySelector("input[name=snake]:checked").value);
   const  qGem= parseInt(document.querySelector("input[name=gem]:checked").value);
   const qFast = parseInt(document.querySelector("input[name=fast]:checked").value);
   const qOxy = parseInt(document.querySelector("input[name=oxy]:checked").value);
   const qGreen = parseInt(document.querySelector("input[name=green]:checked").value);
-  documebt.querySelector("form").removeEventListener("submit", results);
+  document.querySelector("form").removeEventListener("submit", results);
 
-  if ()
+  if (qSnake && qGem && qFast && qOxy && qGreen);
 }
 
